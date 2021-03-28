483b97289540bad7879a625584326d93e3d61a43
IvlevEvgeniy <79964221+IvlevEvgeniy@users.noreply.github.com>
2021-03-28 10:07:59 +0300

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+#define N 3
+using namespace std;
+
+int main() {
+	double x, sum = 1, elem = 1;
+	do {
+
+		cout << "Input x : " << endl;
+		cin >> x;
+		cin.clear();
+	} while (cin.get() != '\n');
+	
+	for (int i = 1; i < N+1; i++)
+	{
+		elem *= -x * x / (2 * i * (2 * i - 1)); 
+		sum += elem;	
+	}
+	cout <<  sum ; 
+
+}
